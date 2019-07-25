cd0c6fd564cb3cb89ff24b028824c90ef6588ec3
jkone27 <jkone27@yahoo.it>
2019-07-25 12:42:43 +0200

    backend now working, to fix frontend -hangs after post

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -12,65 +12,57 @@ open Fulma
 open Shared
 open Thoth.Json
 
-/// The different elements of the completed report.
-type Report =
-    { Location : LocationResponse
-      Crimes : CrimeResponse array }
-
 type ServerState = Idle | Loading | ServerError of string
 
 /// The overall data model driving the view.
 type Model =
-    { Postcode : string
-      ValidationError : string option
+    { 
       ServerState : ServerState
-      Report : Report option }
+      Name: string
+      Names : string list }
 
 /// The different types of messages in the system.
 type Msg =
-    | GetReport
-    | PostcodeChanged of string
-    | GotReport of Report
     | ErrorMsg of exn
+    | PostYourName of string
+    | PostOk of unit
+    | GetNames of string list
+    | NameChanged of string
 
 /// The init function is called to start the message pump with an initial view.
+
+
+let namesDecoder = Decode.Auto.generateDecoder<string list> ()
+
+let getNames (_:unit) = promise {
+    return! Fetch.fetchAs "api/name" namesDecoder []
+}
+
+let postName name = promise {
+    let! postResult = Fetch.postRecord "api/name" { MyName = name } []
+    if not postResult.Ok then 
+        failwith "unable to post!"
+    else
+        ()
+}
+
 let init () =
-    { Postcode = null
-      Report = None
-      ValidationError = None
-      ServerState = Idle }, Cmd.ofMsg (PostcodeChanged "")
+    { 
+      ServerState = Idle
+      Name = ""
+      Names = []
+    }, Cmd.ofPromise getNames () GetNames ErrorMsg
 
-let decoderForLocationResponse = Decode.Auto.generateDecoder<LocationResponse> ()
-let decoderForCrimeResponse = Decode.Auto.generateDecoder<CrimeResponse array>()
-let decoderForWeather = Decode.Auto.generateDecoder<WeatherResponse>()
-
-let getResponse postcode = promise {
-    let! location = Fetch.fetchAs<LocationResponse> (sprintf "/api/distance/%s" postcode) decoderForLocationResponse []
-    let! crimes = Fetch.tryFetchAs (sprintf "api/crime/%s" postcode) decoderForCrimeResponse [] |> Promise.map (Result.defaultValue [||])
-
-    (* Task 4.5 WEATHER: Fetch the weather from the API endpoint you created.
-       Then, save its value into the Report below. You'll need to add a new
-       field to the Report type first, though! *)
-    return { Location = location; Crimes = crimes } }
 
 /// The update function knows how to update the model given a message.
 let update msg model =
     match model, msg with
-    | { ValidationError = None; Postcode = postcode }, GetReport ->
-        { model with ServerState = Loading }, Cmd.ofPromise getResponse postcode GotReport ErrorMsg
-    | _, GetReport -> model, Cmd.none
-    | _, GotReport response ->
-        { model with
-            ValidationError = None
-            Report = Some response
-            ServerState = Idle }, Cmd.none
-    | _, PostcodeChanged p ->
-        { model with
-            Postcode = p
-            (* Task 2.2 Validation. Use the Validation.validatePostcode function to implement client-side form validation.
-               Note that the validation is the same shared code that runs on the server! *)
-            ValidationError = None }, Cmd.none
     | _, ErrorMsg e -> { model with ServerState = ServerError e.Message }, Cmd.none
+    | _, NameChanged name -> { model with Name = name }, Cmd.none
+    |_, PostYourName name -> 
+        model, Cmd.ofPromise postName name PostOk ErrorMsg
+    |_, PostOk _ -> model, Cmd.none
+    |_, GetNames names -> { model with Names = names}, Cmd.none
 
 [<AutoOpen>]
 module ViewParts =
@@ -85,60 +77,55 @@ module ViewParts =
                 (Heading.h2 [ ] [ str title ] :: content)
         ]
 
-    let crimeTile crimes =
-        let cleanData = crimes |> Array.map (fun c -> { c with Crime = c.Crime.[0..0].ToUpper() + c.Crime.[1..].Replace('-', ' ') } )
-        basicTile "Crime" [ ] [
-            barChart
-                [ Chart.Data cleanData
-                  Chart.Width 600.
-                  Chart.Height 500.
-                  Chart.Layout Vertical ]
-                [ xaxis [ Cartesian.Type "number" ] []
-                  yaxis [ Cartesian.Type "category"; Cartesian.DataKey "Crime"; Cartesian.Width 200. ] []
-                  bar [ Cartesian.DataKey "Incidents" ] [] ]
-        ]
+    let nameInput model dispatch = 
+        Field.div [] [
+                    Label.label [] [ str "Name" ]
+                    Control.div [ Control.HasIconLeft; Control.HasIconRight ] [
+                        Input.text
+                            [ Input.Placeholder "Ex: whatever"
+                              Input.Value model.Name
+                              Input.Props [ OnChange (fun ev -> dispatch (NameChanged !!ev.target?value))] ]
+                        Fulma.Icon.icon [ Icon.Size IsSmall; Icon.IsLeft ] [ Fa.i [ Fa.Solid.Home ] [] ]
+                    ]
+                ]
 
-    let getBingMapUrl latLong =
-        sprintf "https://www.bing.com/maps/embed?h=400&w=800&cp=%f~%f&lvl=11&typ=s&FORM=MBEDV8" latLong.Latitude latLong.Longitude
-
-    let bingMapTile (latLong:LatLong) =
-        basicTile "Map" [ Tile.Size Tile.Is12 ] [
-            iframe [
-                Style [ Height 410; Width 810 ]
-                (* Task 3.1 MAPS: Use the getBingMapUrl function to build a valid maps URL using the supplied LatLong.
-                   You can use it to add a Src attribute to this iframe. *)
-            ] [ ]
-        ]
-
-    let weatherTile weatherReport =
-        childTile "Weather" [
-            Level.level [ ] [
-                Level.item [ Level.Item.HasTextCentered ] [
-                    div [ ] [
-                        Level.heading [ ] [
-                            Image.image [ Image.Is128x128 ] [
-                                img [ Src(sprintf "https://www.metaweather.com/static/img/weather/%s.svg" weatherReport.WeatherType.Abbreviation) ]
-                            ]
-                        ]
-                        Level.title [ ] [
-                            Heading.h3 [ Heading.Is4; Heading.Props [ Style [ Width "100%" ] ] ] [
-                                (* Task 4.8 WEATHER: Get the temperature from the given weather report
-                                   and display it here instead of an empty string. *)
-                                str ""
-                            ]
+    let postButton model dispatch =
+        Field.div [ Field.IsGrouped ] [
+                    Level.level [ ] [
+                        Level.left [] [
+                            Level.item [] [
+                                Button.button
+                                    [ Button.IsFullWidth
+                                      Button.Color IsPrimary
+                                      Button.OnClick (fun _ -> dispatch (PostYourName(model.Name)))
+                                      Button.IsLoading (model.ServerState = ServerState.Loading) ]
+                                    [ str "Post" ]]
                         ]
                     ]
                 ]
-            ]
-        ]
-    let locationTile model =
-        childTile "Location" [
-            div [ ] [
-                Heading.h3 [ ] [ str model.Location.Location.Town ]
-                Heading.h4 [ ] [ str model.Location.Location.Region ]
-                Heading.h4 [ ] [ sprintf "%.1fKM to London" model.Location.DistanceToLondon |> str ]
-            ]
-        ]
+
+    let serverError error =
+        Field.div [] [
+                        Tag.list [ Tag.List.HasAddons; Tag.List.IsCentered ] [
+                            Tag.tag [ Tag.Color Color.IsDanger; Tag.Size IsMedium ] [
+                                str error
+                            ]
+                        ]
+                    ]
+
+    let header model =
+        Heading.h1 [ ] [
+                        (let name = 
+                            match model.Name with
+                            |"" -> "we don't know your name"
+                            |n -> sprintf "your name is %s" n
+                        str name)
+                    ]
+
+    let loadNames model =
+        let namesElements = model.Names |> Seq.map (fun n -> Field.p [] [ str n ])
+        Field.div [] namesElements
+
 
 
 /// The view function knows how to render the UI given a model, as well as to dispatch new messages based on user actions.
@@ -148,82 +135,19 @@ let view model dispatch =
             Hero.body [ ] [
                 Container.container [ Container.IsFluid
                                       Container.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ] [
-                    Heading.h1 [ ] [
-                        str "UK Location Data Mashup"
-                    ]
+                    header model 
                 ]
             ]
         ]
 
         Container.container [] [
-            yield
-                Field.div [] [
-                    Label.label [] [ str "Postcode" ]
-                    Control.div [ Control.HasIconLeft; Control.HasIconRight ] [
-                        Input.text
-                            [ Input.Placeholder "Ex: EC2A 4NE"
-                              Input.Value model.Postcode
-                              Input.Modifiers [ Modifier.TextTransform TextTransform.UpperCase ]
-                              Input.Color (if model.ValidationError.IsSome then Color.IsDanger else Color.IsSuccess)
-                              Input.Props [ OnChange (fun ev -> dispatch (PostcodeChanged !!ev.target?value)); onKeyDown KeyCode.enter (fun _ -> dispatch GetReport) ] ]
-                        Fulma.Icon.icon [ Icon.Size IsSmall; Icon.IsLeft ] [ Fa.i [ Fa.Solid.Home ] [] ]
-                        (match model with
-                         | { ValidationError = Some _ } ->
-                            Icon.icon [ Icon.Size IsSmall; Icon.IsRight ] [ Fa.i [ Fa.Solid.Exclamation ] [] ]
-                         | { ValidationError = None } ->
-                            Icon.icon [ Icon.Size IsSmall; Icon.IsRight ] [ Fa.i [ Fa.Solid.Check ] [] ])
-                    ]
-                    Help.help
-                       [ Help.Color (if model.ValidationError.IsNone then IsSuccess else IsDanger) ]
-                       [ str (model.ValidationError |> Option.defaultValue "") ]
-                ]
-            yield
-                Field.div [ Field.IsGrouped ] [
-                    Level.level [ ] [
-                        Level.left [] [
-                            Level.item [] [
-                                Button.button
-                                    [ Button.IsFullWidth
-                                      Button.Color IsPrimary
-                                      Button.OnClick (fun _ -> dispatch GetReport)
-                                      Button.Disabled (model.ValidationError.IsSome)
-                                      Button.IsLoading (model.ServerState = ServerState.Loading) ]
-                                    [ str "Submit" ]
-                            ]
-                        ]
-                    ]
-                ]
-
+            yield loadNames model
+            yield nameInput model dispatch
+            yield postButton model dispatch 
             match model with
-            | { Report = None; ServerState = (Idle | Loading) } -> ()
             | { ServerState = ServerError error } ->
-                yield
-                    Field.div [] [
-                        Tag.list [ Tag.List.HasAddons; Tag.List.IsCentered ] [
-                            Tag.tag [ Tag.Color Color.IsDanger; Tag.Size IsMedium ] [
-                                str error
-                            ]
-                        ]
-                    ]
-            | { Report = Some model } ->
-                yield
-                    Tile.ancestor [ ] [
-                        Tile.parent [ Tile.Size Tile.Is12 ] [
-                            bingMapTile model.Location.Location.LatLong
-                        ]
-                    ]
-                yield
-                    Tile.ancestor [ ] [
-                        Tile.parent [ Tile.IsVertical; Tile.Size Tile.Is4 ] [
-                            locationTile model
-                            (* Task 4.6 WEATHER: Generate the view code for the weather tile
-                               using the weatherTile function, supplying the weather report
-                               from the model, and include it here as part of the list *)
-                        ]
-                        Tile.parent [ Tile.Size Tile.Is8 ] [
-                            crimeTile model.Crimes
-                        ]
-                  ]
+                yield serverError error 
+            |_ -> yield Field.div [] []
         ]
 
         br [ ]
